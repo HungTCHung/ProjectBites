4d321fb511e002bd6629cce062bc1d33a436fc05
folks <xuatkich2423@gmail.com>
2024-05-03 23:01:52 +0700

    init

--- ProjectBites/reactjavascript/my-app/src/routes/PrivateRoute.js
+++ ProjectBites/reactjavascript/my-app/src/routes/PrivateRoute.js
@@ -2,27 +2,34 @@ import React, { useContext } from "react";
 import { Route, Redirect, useHistory } from "react-router-dom";
 import { UserContext } from "../context/userContext";
 import { Router } from "react-router-dom/cjs/react-router-dom.min";
-
+import Home from "../components/home/home";
 const PrivateRoute = (props) => {
   let history = useHistory();
   const { user } = useContext(UserContext);
 
-  if (user && user.isAuthenticated) {
+  if (user.isAuthenticated === true) {
     return (
       <>
         <Route path={props.path}>{props.component}</Route>
       </>
     );
+  } else {
+    return (
+      <>
+        <Route path="/">
+          <Home />
+        </Route>
+        
+      </>
+    );
   }
-  if (!user && user.isAuthenticated !== 0) {
-    history.push("/");
-  }
+
   // if (user.isAuthenticated !== 0) {
   //   history.push("/");
   // }
-  if (user && user.isAuthenticated) {
-    history.push("/write-post");
-  }
+  // if (user && user.isAuthenticated) {
+  //   history.push("/write-post");
+  // }
 };
 
 export default PrivateRoute;
